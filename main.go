38de9a3c51490885160bad6780655b773b2972fd
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-04-23 17:01:33 +0000

    comments and gofmt

--- main.go
+++ main.go
@@ -1,80 +1,82 @@
 package main
 
 import (
-            "github.com/go-martini/martini"
-            "labix.org/v2/mgo"
-            "errors"
-            "flag"
-            "fmt"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/go-martini/martini"
+	"labix.org/v2/mgo"
 )
 
-var mgoHost *string = flag.String("host","localhost","MongoDB host string.")
-var mgoDb *string = flag.String("db","test","MongoDB database to connect to.")
+var mgoHost *string = flag.String("host", "localhost", "MongoDB host string.")
+var mgoDb *string = flag.String("db", "test", "MongoDB database to connect to.")
 
 func getRequiredCollections() map[string]bool {
-    return map[string]bool {
-        "contentTypes": true,
-        "admins": true,
-    }
+	return map[string]bool{
+		"contentTypes": true,
+		"admins":       true,
+	}
 }
 
 func getSession(host string) *mgo.Session {
-    //Attempt to dial host
-    s, err := mgo.Dial(host)
+	//Attempt to dial host
+	s, err := mgo.Dial(host)
 
-    //If we can't connect, panic
-    if err != nil {
-        panic(err)
-    }
+	//If we can't connect, panic
+	if err != nil {
+		panic(err)
+	}
 
-    //At this point we're fine
-    s.SetMode(mgo.Monotonic, true)
-    return s
+	//At this point we're fine
+	s.SetMode(mgo.Monotonic, true)
+	return s
 }
 
 func main() {
 
-    //Parse flags
-    flag.Parse()
+	//Parse flags
+	flag.Parse()
 
-    //Create mongo session
-    session := getSession(*mgoHost)
-    defer session.Close()
+	//Create mongo session
+	session := getSession(*mgoHost)
+	defer session.Close()
 
-    //Connect to the database
-    d := session.DB(*mgoDb)
+	//Connect to the database
+	d := session.DB(*mgoDb)
 
-    //Attempt to get collection names. If we can't, we connected to a bad DB
-    collectionNames, err := d.CollectionNames()
+	//Attempt to get collection names. If we can't, we connected to a bad DB
+	collectionNames, err := d.CollectionNames()
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    req := getRequiredCollections()
+	req := getRequiredCollections()
 
-    for _, el := range collectionNames {
-        if val, ok := req[el]; val && ok {
-            req[el] = false
-        }
-    }
+	//Make sure that have all required collections
+	for _, el := range collectionNames {
+		if val, ok := req[el]; val && ok {
+			req[el] = false
+		}
+	}
 
-    for idx, notFound := range req {
-        if notFound {
-            errStr := fmt.Sprintf("Database '%s' does not have required collection '%s'.", d.Name, idx)
-            err = errors.New(errStr)
-            break
-        }
-    }
+	//If we don't have one of the required collections, panic
+	for idx, notFound := range req {
+		if notFound {
+			errStr := fmt.Sprintf("Database '%s' does not have required collection '%s'.", d.Name, idx)
+			err = errors.New(errStr)
+			break
+		}
+	}
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    //Create martini and serve
-    m := martini.Classic()
-    m.Get("/", func() string {
-        return "Hello world!"
-    })
-    m.Run()
+	//Create martini and serve
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return "Hello world!"
+	})
+	m.Run()
 }
