1078e113659035fbe8f6e2755dfa1c43c4f84b0a
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-05-06 11:01:42 +0000

    now works as a falcore hello world server

--- tingd/tingd.go
+++ tingd/tingd.go
@@ -4,6 +4,8 @@ import (
     "os"
     "fmt"
     "strings"
+    "strconv"
+    "net/http"
     "github.com/fitstar/falcore"
     "github.com/docopt/docopt-go"
 )
@@ -24,23 +26,40 @@ func usage() string {
     return fmt.Sprintf(`tingd.
 
         Usage:
-            tingd [--host=<hostname>] [--db=<dbname]
+            tingd [--host=<hostname>] [--db=<dbname>] [--port=<port>]
             tingd --help
             tingd --version
 
         Options:
             -h | --host     MongoDB host string [default: localhost].
             -d | --db       MongoDB database string [default: %s].
-            -p | --port     The port to listen on [default: 8000].
+            -p | --port     The port to listen on [default: 5000].
             --help          Show this screen.
             --version       Show version.`, workingDir())
 }
 
-func initFalcore(port int) {
+func pipeline() *falcore.Pipeline {
+    p := falcore.NewPipeline()
+    p.Upstream.PushBack(helloFilter)
+    return p
+}
 
+func server(port int, pipeline *falcore.Pipeline) *falcore.Server {
+    return falcore.NewServer(port, pipeline)
 }
 
 func main() {
     args, _ := docopt.Parse(usage(), nil, true,fmt.Sprintf("tingd %s", version), false)
-    if
+    port, err := strconv.Atoi(args["--port"].(string))
+    if err != nil {
+        panic(err)
+    }
+    server := server(port, pipeline())
+    if err := server.ListenAndServe(); err != nil {
+        panic(err)
+    }
 }
+
+var helloFilter = falcore.NewRequestFilter(func(req *falcore.Request) *http.Response {
+    return falcore.StringResponse(req.HttpRequest, 200, nil, "hello world!")
+})
