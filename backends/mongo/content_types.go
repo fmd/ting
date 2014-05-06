b27ea2242068d8864f2f85b4b099d3eee473e7e7
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-05-06 13:09:27 +0000

    updated backedns

--- backends/mongo/content_types.go
+++ backends/mongo/content_types.go
@@ -1,9 +1,22 @@
 package mongo
 
-func (r *Repo) StructureType(contentType string) {
+func (r *Repo) StructureType(contentType string, structure []byte) {
 
 }
 
-func (r *Repo) ContentTypes() {
-    
+func (r *Repo) ContentTypes() ([]string, error) {
+    n, err := r.Db.CollectionNames()
+    if err != nil {
+        return nil, err
+    }
+
+    names := make([]string,1)
+
+    for _, name := range n {
+        if name != "structures" && name != "system.indexes" {
+            names = append(names, name)
+        }
+    }
+
+    return names, nil
 }
