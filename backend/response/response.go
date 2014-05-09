5a4b6a01ea2e1840bddeddab909d612efd02fc77
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-05-09 12:48:39 +0000

    added backend response class

---
+++ backend/response/response.go
@@ -0,0 +1,29 @@
+package response
+
+//R is the struct that all calls to Backend functions returns.
+type R struct {
+    Data   interface{}
+    Status string
+    Error  error
+}
+
+func Error(err error) *R {
+    r := &R{}
+    r.Error = err
+    r.Status = "error"
+    return r
+}
+
+func Success(data interface{}) *R {
+    r := &R{}
+    r.Status = "success"
+    r.Data = data
+    return r
+}
+
+func Fail(data interface{}) *R {
+    r := &R{}
+    r.Status = "fail"
+    r.Data = data
+    return r
+}
